fc57bb5527c81a233031514f6e12c00b27bedbed
kamruzzaman-insights <kamruzzaman.insights@gmail.com>
2026-01-05 04:53:05 +0600

    learning basic dim

---
+++ js/first.js
@@ -0,0 +1,12 @@
+const alli = document.getElementsByTagName('li');
+console.log(alli);
+
+const allTitle = document.getElementsByClassName('section-title');
+console.log(allTitle);
+
+const secondSection = document.getElementById('second-section');
+console.log(secondSection);
+
+secondSection.style.color = 'violet';
+secondSection.style.backgroundColor = 'steelblue';
+
